9c3fe99d7792bde3402bc3e3d9f47641f4d3d652
Diwas Rai <diwasrai38@yahoo.com>
2025-11-30 13:05:08 +0000

    add echo command

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
@@ -14,15 +15,23 @@ func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		latestCommand := command[:len(command)-1]
-		if latestCommand == "exit" {
+
+		latestInput := strings.Split(input[:len(input)-1], " ")
+		command := latestInput[0]
+		args := latestInput[1:]
+
+		switch command {
+		case "echo":
+			fmt.Println(strings.Join(args, " "))
+		case "exit":
 			os.Exit(0)
+		default:
+			fmt.Println(command + ": command not found")
 		}
-		fmt.Println(latestCommand + ": command not found")
 	}
 }
